c6be1e44e4ff41ef8c34dc585c27a4b63f612aa6
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-07-10 20:56:24 +0200

    🥯 Update station page

--- OCPP.Core.UI/src/app/stations/page.tsx
+++ OCPP.Core.UI/src/app/stations/page.tsx
@@ -17,17 +17,34 @@ export default function Home() {
 
   return (
     <section className="space-y-5">
-      <div className="w-36">
-        <Select>
-          <SelectTrigger>
-            <SelectValue placeholder="Status" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="offline">Offline</SelectItem>
-            <SelectItem value="charging">Charging</SelectItem>
-            <SelectItem value="available">Available</SelectItem>
-          </SelectContent>
-        </Select>
+      <div className="flex gap-3">
+        <div className="w-36">
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Status" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="offline">Offline</SelectItem>
+              <SelectItem value="charging">Charging</SelectItem>
+              <SelectItem value="available">Available</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="w-36">
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="OCPP" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="2.0">OCPP 2.0.1</SelectItem>
+              <SelectItem value="1.6">OCPP 1.6</SelectItem>
+              <SelectItem value="1.5">OCPP 1.5</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      <div className="w-full h-80 bg-slate-600 rounded-lg flex justify-center items-center">
+        Map
       </div>
       <div>
         <DataTable data={stations} columns={columns} />
