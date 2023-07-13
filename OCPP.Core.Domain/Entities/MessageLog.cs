c33cae649f8dc8434acff308d78e1f018758f4f1
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-07-13 16:42:13 +0200

    🚕 Handle Message Log

--- OCPP.Core.Domain/Entities/MessageLog.cs
+++ OCPP.Core.Domain/Entities/MessageLog.cs
@@ -1,13 +1,35 @@
-﻿#nullable disable
-namespace OCPP.Core.Domain.Entities;
+﻿namespace OCPP.Core.Domain.Entities;
 
-public partial class MessageLog
+public sealed partial class MessageLog
 {
   public int LogId { get; set; }
   public DateTime LogTime { get; set; }
-  public string ChargePointId { get; set; }
+  public string ChargePointId { get; set; } = null!;
   public int? ConnectorId { get; set; }
-  public string Message { get; set; }
-  public string Result { get; set; }
-  public string ErrorCode { get; set; }
+  public string Message { get; set; } = null!;
+  public string Result { get; set; } = null!;
+  public string ErrorCode { get; set; } = null!;
+
+  private MessageLog() { }
+
+  public MessageLog(DateTime logTime, string chargeStationId, int? connectorId, string message, string result, string errorCode)
+  {
+    LogTime = logTime;
+    ChargePointId = chargeStationId;
+    ConnectorId = connectorId;
+    Message = message;
+    Result = result;
+    ErrorCode = errorCode;
+  }
+
+  public static MessageLog Create(DateTime logTime, string chargeStationId, int? connectorId, string message, string result, string errorCode)
+  {
+    return new MessageLog(
+      logTime,
+      chargeStationId,
+      connectorId,
+      message,
+      result,
+      errorCode);
+  }
 }
