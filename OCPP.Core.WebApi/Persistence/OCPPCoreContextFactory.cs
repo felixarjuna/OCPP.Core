6379c0904df9e7c822c76f9a3d0c2d04ca1b5428
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-07-21 13:43:54 +0200

    🧂 Add Design Time Context Factory and Databases

---
+++ OCPP.Core.WebApi/Persistence/OCPPCoreContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace OCPP.Core.WebApi.Persistence;
+
+public class OCPPCoreContextFactory : IDesignTimeDbContextFactory<OCPPCoreDbContext>
+{
+  public OCPPCoreDbContext CreateDbContext(string[] args)
+  {
+    IConfiguration _configuration = new ConfigurationBuilder()
+      .SetBasePath(AppContext.BaseDirectory)
+      .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+      .Build();
+    string? connectionString = _configuration.GetConnectionString("SQLite");
+    Console.WriteLine(connectionString);
+
+    var builder = new DbContextOptionsBuilder<OCPPCoreDbContext>();
+    builder.UseSqlite(connectionString);
+
+    return new OCPPCoreDbContext(builder.Options);
+  }
+}
